af70646c75bf232086b49979011a10e13f0caeaa
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-22 15:21:23 +0200

    resolve functions

--- server/index.js
+++ server/index.js
@@ -8,23 +8,30 @@ const schema = buildSchema(`
         url: String
         description: String!
     }
-
+    type User{
+        _id: Int!,
+        username:String!
+    }
     type Query {
-        links: [Link!]!
+        allLinks: [Link!]!
         link(_id: Int): Link!
+        allUsers: [User!]!
+        user(_id: Int) : User!
     }
 `);
 const links =[
     {_id: 0 ,url: 'https://sumithpd.com', description: 'my website' },
     {_id: 1 ,url: 'https://examle1.com', description: 'a website 1' },
-]
-
+];
+const users =[
+    {_id: 0 , username: 'user1'},
+    {_id: 1 , username: 'user2'},
+];
 const root = {
-  links: () => links,
-  link: ({_id}) =>{
-      const link= links.filter(i=> i._id === _id)[0];
-      return link;
-  },
+  allLinks: () => links,
+  link: ({_id}) => links.filter(i=> i._id === _id)[0],
+  allUsers: () => users,
+  user: ({_id}) => users.filter(u=> u._id === _id)[0],
 };
 
 const app = express();
