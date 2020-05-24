21589fb5f567e42b4ad7285151737044dde26b52
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-05-24 10:48:24 +0200

    recursive relationships

--- server/index.js
+++ server/index.js
@@ -10,11 +10,19 @@ const typeDefs = `
     url: String!
     description: String
     author: User!
+    comments: [Comment]
   }
   type User {
     id: Int! @unique
     username: String!
     about: String
+  }
+  type Comment {
+    id: Int! @unique
+    parent: Comment
+    comments: [Comment]
+    author: User!
+    content: String!
   }
   type Query {
     allLinks: [Link]
@@ -22,18 +30,25 @@ const typeDefs = `
     allUsers: [User]
     user(id: Int!): User
   }
-`
+`;
 
 const links = [
-  { id: 0, author: 0, url: 'https://example.com', description: 'example site' },
-  { id: 1, author: 1, url: 'https://google.com', description: 'Google site' },
-]
+  { id: 0, author: 0, url: 'https://Sumithpd.com', description: 'My site' ,comments:[0] },
+  { id: 1, author: 1, url: 'https://google.com', description: 'Google site',comments:[2,4] },
+];
 
 const users = [
   { id: 0, username: 'user1', about: 'user about1' },
   { id: 1, username: 'user2', about: 'user about2' },
-]
+];
 
+const commentsList = [
+  { id: 0, parent: null, author: 0,content: 'Comment 1' },
+  { id: 1, parent: 0, author: 1,content: 'Comment 2' },
+  { id: 2, parent: null, author: 0,content: 'Comment 3' },
+  { id: 3, parent: 2, author: 1,content: 'Comment 4' },
+  { id: 4, parent: null, author: 1,content: 'Comment 5' },
+];
 const resolvers = {
   Query: {
     allLinks: () => links,
@@ -43,15 +58,17 @@ const resolvers = {
   },
   Link: {
     author: ({ author }) => find(users, { id: author }),
+    comments: ({ comments }) =>
+    comments.map(i => find(commentsList, { id: i })),
   },
-}
+};
 
-const schema = makeExecutableSchema({ typeDefs, resolvers })
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const app = express()
+const app = express();
 const graphqlHTTPOptions = {
   schema,
   graphiql: true,
-}
-app.use('/graphql', graphqlHTTP(graphqlHTTPOptions))
-app.listen(4000, () => console.log('🏃‍♂️ server is running on port 4000'))
+};
+app.use('/graphql', graphqlHTTP(graphqlHTTPOptions));
+app.listen(4000, () => console.log('🏃‍♂️ server is running on port 4000'));
